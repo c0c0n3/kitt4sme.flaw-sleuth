f5dd39dc43478bdce8996b3b218183c4be6a05bf
Andrea Falconi <andrea.falconi@gmail.com>
2022-03-30 22:37:14 +0200

    bring over kitt4sme.anomaly ml models to new home.

--- flawsleuth/runner.py
+++ flawsleuth/runner.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+from flawsleuth.ai import predict
 from flawsleuth.timeseries import fetch_data_frame
 
 
@@ -7,3 +8,7 @@ def run():
     st.write("Here's an entity series from Quantum Leap:")
     df = fetch_data_frame()
     st.write(df)
+
+    st.write("Is there a flaw?")
+    answer = predict(12.3)
+    st.write(answer)
